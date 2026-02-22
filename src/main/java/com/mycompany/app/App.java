e6af857d414d458cb7858ab76b7cfee9b362820e
Anushree <anushreek2501@gmail.com>
2026-02-22 22:35:38 +0530

    dependency added in pom file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,23 @@
 package com.mycompany.app;
+import org.apache.commons.lang3.StringUtils;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 		System.out.println("Added one line of code to the file");
 		System.out.println("Line added to check github actions");
+		
+		System.out.println("Testing commons-lang dependency");
+		
+		String input = "   devops engineer   ";
+		System.out.println("Original String = " + input);
+		
+		String trimmed = StringUtils.trim(input);
+		System.out.println("Trimmed String = " + trimmed);
+		
+		String upper = StringUtils.upperCase(trimmed);
+		System.out.println("Uppercase: " + upper);
+		
+		
     }
 }
