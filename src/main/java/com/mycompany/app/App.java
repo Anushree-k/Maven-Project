9c5ac0bfa58867fccc4e603036f0929abf55b902
Anushree <anushreek2501@gmail.com>
2026-02-22 18:17:35 +0530

    First commit - 2 lines of code

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+		System.out.println("Added one line of code to the file");
+    }
+}
