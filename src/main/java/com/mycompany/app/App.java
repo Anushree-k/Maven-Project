93d2d9adbd8c3eeb7e09170ab8da83984f3dd373
Anushree <anushreek2501@gmail.com>
2026-02-22 20:16:36 +0530

    modified app.java to check github actions trigger on push

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 		System.out.println("Added one line of code to the file");
+		System.out.println("Line added to check github actions");
     }
 }
